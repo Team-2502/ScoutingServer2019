de24013b22eb16a9322529a4cc558b479fa4586b
Ryan Alexander <ryandalex1@outlook.com>
2019-07-28 13:16:42 -0500

    Decompress TIMD timeline to a list of dictionaries that represent all actions taken

--- decompressor.py
+++ decompressor.py
@@ -58,3 +58,57 @@ TEMP_TIMD_COMP_VALUES = {
     'ag': 'defenseEnd',
 }
 
+very_cool_timd = 'A37B1923CaDaEcFfGfHkIlJt|KrLyM146NlOvPf,KqLpM140Nl,KrLyM138NlOuPt,KqLoM135Nl,KrLyM134NlOsPf,KqLpM132Nm,KrLzM130NmOsQwPt,KqLoM128Nm,KaaLoM120NnPt,KqLoM116Nm,KrLzM115NmOsQxPt,KacM112,KadM110|M108RvSvTt'
+def decompress_timd(temp_timd):
+    header, timeline, climb = temp_timd.split("|")
+    print(header)
+    print(timeline)
+    decompress_timeline(timeline)
+    print(climb)
+
+def decompress_timeline(timeline):
+    compressed_timeline = timeline.split(",")
+    print(compressed_timeline)
+    decompressed_timeline = []
+    for action in compressed_timeline:
+        decompressed_action = {}
+        last_key_index = 0
+        for index, character in enumerate(action):
+            # First character
+            if index == 0:
+                last_key_index = 0
+            # Last character
+            if index == len(action) - 1:
+                compressed_value = action[last_key_index + 1:]
+                value = decompress_value(compressed_value)
+                compressed_key = action[last_key_index]
+                key = TEMP_TIMD_COMP_KEYS[compressed_key]
+                decompressed_action[key] = value
+                last_key_index = index
+            # TODO Prepare for having keys that are longer than one character
+            # Next capital letter key
+            elif character.isupper():
+                # Value from end of last key to this key
+                compressed_value = action[last_key_index + 1:index]
+                value = decompress_value(compressed_value)
+                compressed_key = action[last_key_index]
+                key = TEMP_TIMD_COMP_KEYS[compressed_key]
+                decompressed_action[key] = value
+                last_key_index = index
+        decompressed_timeline.append(decompressed_action)
+    print(decompressed_timeline)
+
+
+def decompress_value(compressed_value):
+    # Value is a compressed string
+    if compressed_value.isalpha():
+        return TEMP_TIMD_COMP_VALUES[compressed_value]
+    # Value is a number, usually a time
+    else:
+        return compressed_value
+
+
+def main():
+    decompress_timd(very_cool_timd)
+
+main()
