d17760a0ff92dc370b9c8201f1bbfed621ac0196
Ryan Alexander <ryandalex1@outlook.com>
2019-08-11 18:48:39 -0500

    Export cycle times to a new worksheet and get rid of the worthless headers

--- export.py
+++ export.py
@@ -5,7 +5,7 @@ import os
 homeDir = os.path.expanduser('~')
 teams = [json.loads(open(os.path.join(homeDir, 'ScoutingServer/cache/teams/', team)).read()) for team in os.listdir(os.path.join(homeDir, 'ScoutingServer/cache/teams')) if team != '.DS_Store']
 
-current_row = 3
+current_row = 2
 for team in teams:
     if len(team.keys()) > 2:
         totals = ([key for key in team['totals'].keys()], 'totals')
@@ -15,7 +15,9 @@ for team in teams:
         rankings = ([key for key in team['rankings'].keys()], 'rankings')
         team_abilities = ([key for key in team['team_abilities'].keys()], 'team_abilities')
         percentages = ([key for key in team['percentages'].keys()], 'percentages')
-        sykes = ([key for key in team['sykesData'].keys()], 'Sykes')
+        sykes = ([key for key in team['sykesData'].keys()], 'sykes')
+
+        cycles = ([key for key in team['cycle_times'].keys()], 'cycles')
 
         headers = [totals, l3ms, SDs, maxes, rankings, team_abilities, percentages]
         if not os.path.exists(os.path.join(os.getcwd(), 'uwu.xlsx')):
@@ -23,20 +25,22 @@ for team in teams:
             ws = wb.active
             ws.title = 'Raw Export'
 
-            ws.cell(row=2, column=1).value = 'Number'
+            ws_cycles = wb.create_sheet("Cycle Export")
+
+            ws.cell(row=1, column=1).value = 'Number'
+            ws_cycles(row=1, column=1).value = 'Number'
 
             current_column = 2
 
-            for header in headers:
-                ws.merge_cells(start_row=1, end_row=1, start_column=current_column,  end_column=current_column + len(header[0]) - 1)
-                ws.cell(row=1, column=current_column).value = header[1]
-                current_column += len(header[0])
-
-            current_column = 2
             for header in headers:
                 for key in header[0]:
-                    ws.cell(row=2, column=current_column).value = key
+                    ws.cell(row=1, column=current_column).value = key
                     current_column += 1
+
+            current_column = 2
+            for key in cycles[0]:
+                ws_cycles.cell(row=1, column=current_column).value = key
+                current_column += 1
 
             wb.save(filename='uwu.xlsx')
 
@@ -48,6 +52,11 @@ for team in teams:
             for key in header[0]:
                 ws.cell(row=current_row, column=current_column).value = team[header[1]][key]
                 current_column += 1
+
+        current_column = 2
+        for key in cycles[0]:
+            ws.cell(row=current_row, column=current_column).value = team['cycle_times'][key]
+            current_column += 1
 
         current_row += 1
         wb.save('uwu.xlsx')
